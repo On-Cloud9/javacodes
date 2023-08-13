6556fd3afcb525e248ed37d26a2db7cb2d17992e
On-cloud7 <priyanka.p62003@gmail.com>
2023-08-13 21:04:53 +0530

    java files added

---
+++ program3.java
@@ -0,0 +1,20 @@
+  // write a program to accept two integers and display their product.
+
+import java.util.Scanner;
+public class program3 {
+    public static void main(String[] args){
+        int a,b , res;
+         try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("enter two integers");
+            a= sc.nextInt();
+            b=sc.nextInt();
+        }
+        res = a*b;
+        System.out.println(" the product of two integers is\n " + res );
+
+
+    }
+    
+}
+
+
---
+++ program8.java
@@ -0,0 +1,23 @@
+// write a program to swap two numbers.
+
+import java.util.Scanner;
+public class program8 {
+    public static void main(String[]args){
+        int a,b, temp;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter two integers");
+            a=sc.nextInt();
+            b = sc.nextInt();
+        }
+        System.out.println("before swapping "+a +" " +b);
+        temp =a;
+        a=b;
+        b=temp;
+        System.out.println("After swapping "+a +" " +b);
+
+
+
+    }
+    
+}
+
