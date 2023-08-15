964d4e26295f832f9f3704a68dc8c97ac8c51541
On-cloud7 <priyanka.p62003@gmail.com>
2023-08-15 08:14:32 +0530

    m

--- program12.java
+++ program12.java
@@ -3,8 +3,9 @@ public class program12 {
     public static void main(String[]args){
         int i,n;
         System.out.println("Enter integers");
-        Scanner sc =new Scanner(System.in);
-        n =sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            n =sc.nextInt();
+        }
         for(i=1;i<=n;i++){
             System.out.println(i);
         }
